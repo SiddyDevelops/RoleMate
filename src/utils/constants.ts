fd786123357ee959ea5e3ab225062ff233ac8d07
Siddharth Singh <siddhartsingh2002@gmail.com>
2023-12-31 11:43:40 +0530

    Added Prisma DB & Migrates --init

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+// Types of User
+const CUSTOMER = "Customer";
+const IT_SYSTEM_ADMIN = "IT System Admin";
+const DEVELOPER = "Developer";
+const SALES_CONSULTANT = "Sales Consultant";
+const HR = "HR";
+
+// Access To
+const COPORATE_NETWORK = "Corporate Network";
+const EMAIL = "Email";
+const CUSTOMER_DB = "Customer DB";
+const UNIX = "Unix";
+const EMPLOYEE_INFO = "Employees info";
